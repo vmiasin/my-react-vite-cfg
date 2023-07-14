1a98af87967075f4c3ba91778e246d0a37dbe6bf
v.miasin <v.miasin@kokoc.tech>
2023-07-14 16:39:32 +0300

    adds mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import { useState } from 'react';
-import './App.scss';
+import { Center, Flex, Button, Title } from '@mantine/core';
+import { useViewportSize } from '@mantine/hooks';
 
 function App() {
   const [count, setCount] = useState(0);
+  const { height } = useViewportSize();
 
   return (
-    <>
-      <h1>Vite + React + TS + SCSS + ESLint + Prettier</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-      </div>
-    </>
+    <Center h={height}>
+      <Flex mih={height} gap="md" justify="center" align="center" direction="column" wrap="wrap">
+        <Title order={1} color="cyan.9">
+          Vite + React + TS + SCSS + Mantine + ESLint + Prettier
+        </Title>
+        <Button
+          onClick={() => setCount((count) => count + 1)}
+          variant="gradient"
+          gradient={{ from: 'indigo', to: 'cyan' }}
+        >
+          count is {count}
+        </Button>
+      </Flex>
+    </Center>
   );
 }
 
